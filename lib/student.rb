185e215e3cebcfb144461109faf0db9ec2ed7fef
Dave Winniford <dwinniford@gmail.com>
2020-02-03 18:41:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
   end
 
   def self.create_from_collection(students_array)
