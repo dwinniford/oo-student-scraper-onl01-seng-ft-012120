a9d4eed880c8cd9589fd7ba3e19601921b009d58
Dave Winniford <dwinniford@gmail.com>
2020-02-03 15:32:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     binding.pry
-   #  all students doc.css("div.student-card")
+   
     
   end
 
@@ -15,3 +15,9 @@ class Scraper
   end
 
 end
+
+
+#  all students doc.css("div.student-card")
+# name  
+# location
+# profile_url
